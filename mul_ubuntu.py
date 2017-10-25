c85427dae87344ef43229756173d8fc440b70b68
TimeGone <7ime6one@sina.com>
2017-10-25 15:14:23 +0800

    add batch normalization

--- mul_ubuntu.py
+++ mul_ubuntu.py
@@ -1,34 +1,40 @@
 import tensorflow as tf
 import numpy as np
 
+def swish(x):
+    return  x * tf.nn.sigmoid(x)
+
 summaries_dir = '/tmp/mul_ubuntu'
 
-activator = tf.nn.relu
+activator = tf.nn.tanh
 optimizer = tf.train.AdamOptimizer(learning_rate=0.001)
 
-x_data = np.random.rand(65536, 2) * 0.1
+x_data = np.random.rand(1024, 2)
 y_data = x_data[:,:1] * x_data[:, 1:2]
 
-x_test = np.random.rand(8192, 2) * 0.1
+x_test = np.random.rand(8192, 2)
 y_test = x_test[:,:1] * x_test[:,1:2]
 
 X = tf.placeholder(tf.float32)
 Y = tf.placeholder(tf.float32)
+training = tf.placeholder(tf.bool, name='training')
 
-W1 = tf.Variable(tf.random_normal([2, 200]), name='weight1')
-b1 = tf.Variable(tf.random_normal([200]), name='bias1')
+W1 = tf.Variable(tf.random_normal([2, 50]), name='weight1')
+b1 = tf.Variable(tf.random_normal([50]), name='bias1')
 layer1 = activator(tf.matmul(X, W1) + b1)
+layer1 = tf.contrib.layers.batch_norm(layer1, is_training=training, fused=True)
 # tf.summary.histogram('W1', W1)
 # tf.summary.histogram('b1', b1)
 
-W2 = tf.Variable(tf.random_normal([200, 100]), name='weight2')
-b2 = tf.Variable(tf.random_normal([100]), name='bias2')
+W2 = tf.Variable(tf.random_normal([50, 25]), name='weight2')
+b2 = tf.Variable(tf.random_normal([25]), name='bias2')
 layer2 = activator(tf.matmul(layer1, W2) + b2)
+layer2 = tf.contrib.layers.batch_norm(layer2, is_training=training, fused=True)
 # hypothesis = tf.matmul(layer1, W2) + b2
 # tf.summary.histogram('W2', W2)
 # tf.summary.histogram('b2', b2)
 
-W3 = tf.Variable(tf.random_normal([100, 1]), name='weight3')
+W3 = tf.Variable(tf.random_normal([25, 1]), name='weight3')
 b3 = tf.Variable(tf.random_normal([1]), name='bias3')
 hypothesis = tf.matmul(layer2, W3) + b3
 # tf.summary.histogram('W3', W3)
@@ -40,8 +46,12 @@ accuracy = tf.reduce_mean(tf.cast(relative_bias < 0.01, dtype=tf.float32))
 loss = tf.reduce_sum(tf.square(tf.subtract(hypothesis, Y)))
 tf.summary.scalar('loss', loss)
 
+update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
+
 grads_and_vars = optimizer.compute_gradients(loss)
-train = optimizer.apply_gradients(grads_and_vars)
+with tf.control_dependencies(update_ops):
+    train = optimizer.apply_gradients(grads_and_vars)
+
 for grad_n_var in grads_and_vars:
     tf.summary.histogram("{}-grad".format(grad_n_var[1].name), grad_n_var[0])
     tf.summary.histogram('{}-var'.format(grad_n_var[1].name), grad_n_var[1])
@@ -50,26 +60,30 @@ for grad_n_var in grads_and_vars:
             
 merged = tf.summary.merge_all()
 
-with tf.Session() as sess:
+config = tf.ConfigProto()
+# config = tf.ConfigProto(device_count={"CPU": 12})
+config.intra_op_parallelism_threads = 160
+config.inter_op_parallelism_threads = 160
+with tf.Session(config=config) as sess:
     train_writer = tf.summary.FileWriter(summaries_dir + '/train', sess.graph)
     sess.run(tf.global_variables_initializer())
-    for step in range(800001):
+    for step in range(800000):
 #        x_data = np.concatenate((x_data, np.random.rand(256, 2)), axis=0)
         if x_data.shape[0] > 256:
-            if step % 10 == 0:
+            if step % 4 == 0:
                 np.random.shuffle(x_data)
                 x_data = np.delete(x_data, 1, axis=0)
-        x_data[0:256] = np.random.rand(256, 2) * 0.1
+        x_data[0:256] = np.random.rand(256, 2)
         y_data = x_data[:,:1] * x_data[:, 1:2]
-
-        _, summary = sess.run([train, merged], feed_dict={X: x_data, Y: y_data})
+        
+        _, summary = sess.run([train, merged], feed_dict={X: x_data, Y: y_data, training:True})
         if step % 100 == 0:
-            print(step, sess.run([loss, accuracy], feed_dict={X:x_data, Y:y_data}), sess.run(relative_bias_mean, feed_dict={X:x_test, Y:y_test}), x_data.shape[0])
+            print(step, sess.run([loss, accuracy, relative_bias_mean], feed_dict={X:x_data, Y:y_data, training:False}), sess.run(relative_bias_mean, feed_dict={X:x_test, Y:y_test, training:False}), x_data.shape[0])
             if step >= 5000:
                 train_writer.add_summary(summary, step)
 
-    x_test = np.random.rand(8192, 2) * 0.1
+    x_test = np.random.rand(8192, 2)
     y_test = x_test[:,:1] * x_test[:,1:2] 
-
-    h, a = sess.run([hypothesis, accuracy], feed_dict={X:x_test, Y:y_test})
+    
+    h, a = sess.run([hypothesis, accuracy], feed_dict={X:x_test, Y:y_test, training:False})
     print("\nHypothesis: ", h , "\ny_value", y_test, "\nAccuracy: ", a)
